4e0ffaa609a2e3d9755cebf051b063aa04223ab6
Krishna <anonymous@google.com>
2025-02-14 07:52:18 +0530

    Features added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,65 +2,115 @@ import { Component } from "react"
 import { NavLink } from "react-router-dom"
 import Dark from '../assets/dark.svg'
 import Light from '../assets/light.svg'
-export default class Navbar extends Component{
-    constructor(){
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
+export default class Navbar extends Component {
+    constructor() {
         super();
-        this.state={
-            mode: Dark
+        this.state = {
+            mode: Dark,
+            trans: false
         }
         this.modeHandler = this.modeHandler.bind(this);
     }
-    modeHandler(){
-        if(this.state.mode===Dark){
-            this.setState({mode:Light})
+
+    drawer = () => {
+        if (this.state.trans === false) {
+            this.setState({ trans: true })
         }
-        else{
-            this.setState({mode:Dark})
+        else {
+            this.setState({ trans: false })
         }
     }
-    render(){
-        let {stylec}=this.props;
-        return(
+    modeHandler() {
+        if (this.state.mode === Dark) {
+            this.setState({ mode: Light })
+        }
+        else {
+            this.setState({ mode: Dark })
+        }
+    }
+    render() {
+        let { stylec } = this.props;
+        return (
             <>
-                <nav className="h-14 w-[100vw] flex justify-between items-center fixed top-0 bg-gray-300/50 backdrop-blur-md">
-                <div className="flex gap-4"> 
-                    <div style={this.state.mode===Light?{color:'white'}:{color:'black'}} className="font-bold  text-xl ml-4">NewsMonkey<span className="text-red-500 font-extrabold text-xl">.</span></div>
-                    <div className="flex gap-4  mt-1">
-                        <NavLink to="/business"className={({isActive})=>
-                            isActive?"font-bold ":""
-                        } end>Business</NavLink>
-                        <NavLink to="/entertainment"className={({isActive})=>
-                            isActive?"font-bold ":""
-                        }end>Entertainment</NavLink>
-                        <NavLink to="/health"className={({isActive})=>
-                            isActive?"font-bold ":""
-                        }end>Health</NavLink>
-                        <NavLink to="/science"className={({isActive})=>
-                            isActive?"font-bold ":""
-                        }end>Science</NavLink>
-                        <NavLink to="/technology"className={({isActive})=>
-                            isActive?"font-bold ":""
-                        }end>Technology</NavLink>
-                        <NavLink to="/sports"className={({isActive})=>
-                            isActive?"font-bold ":""
-                        }end>Sports</NavLink>
+                <nav className="lg:invisible">
+                    <div className={`h-94 ${this.state.trans ? 'visible' : 'hidden'}  ${this.state.mode === Light ? "bg-black" : "bg-white"}`}></div>
+                    <div className="h-14 w-[100vw] flex  justify-between items-center z-10 fixed top-0  bg-gray-300/50 backdrop-blur-md">
+                        <div style={this.state.mode === Light ? { color: 'white' } : { color: 'black' }} className="font-bold text-xl ml-4">NewsMonkey<span className="text-red-500 font-extrabold text-xl">.</span></div>
+                        <div className="mr-8 flex gap-4">
+                            <img src={this.state.mode} onClick={() => {
+                                this.modeHandler();
+                                stylec();
+                            }} className="hover:cursor-pointer" alt="" />
+                            <FontAwesomeIcon icon={faBarsStaggered} className="hover:cursor-pointer" onClick={this.drawer} style={this.state.mode === Light ? { color: 'white' } : { color: 'black' }} size="lg" />
+                        </div>
                     </div>
-                </div>
-                    <div className="mr-6 flex gap-4">
-                    {/* <select name="catagory" id="">
-                    </select> */}
-                        <NavLink to='/' end className={({isActive})=>
-                            isActive?"font-bold ":""
+                    <div style={this.state.mode === Light ? { color: 'white' } : { color: 'black' }} className={`flex gap-4 w-[100vw] justify-center text-md p-5 -translate-y-80  items-center fixed transition-transform duration-1000 ${this.state.trans ? "-translate-y-5" : ""} bg-gray-300/50 backdrop-blur-md flex-col`}>
+                        <NavLink to='/' end className={({ isActive }) =>
+                            isActive ? "font-bold " : ``
                         }>Home</NavLink>
-                        <NavLink to='/about'className={({isActive})=>
-                            isActive?"font-bold":""
+                        <NavLink to='/about' className={({ isActive }) =>
+                            isActive ? "font-bold" : ""
                         } end>About</NavLink>
-                        <img src={this.state.mode} onClick={()=>{
+                        <NavLink to="/business" className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        } end>Business</NavLink>
+                        <NavLink to="/entertainment" className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        } end>Entertainment</NavLink>
+                        <NavLink to="/health" className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        } end>Health</NavLink>
+                        <NavLink to="/science" className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        } end>Science</NavLink>
+                        <NavLink to="/technology" className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        } end>Technology</NavLink>
+                        <NavLink to="/sports" className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        } end>Sports</NavLink>
+                    </div>
+                </nav>
+
+
+
+                <nav className="h-14 w-[100vw] invisible lg:visible flex justify-between items-center fixed top-0 bg-gray-300/50 backdrop-blur-md">
+                    <div className="flex gap-4">
+                        <div style={this.state.mode === Light ? { color: 'white' } : { color: 'black' }} className="font-bold text-xl md:text-2xl ml-4">NewsMonkey<span className="text-red-500 font-extrabold text-xl">.</span></div>
+                        <div className="flex gap-4  mt-1">
+                            <NavLink to="/business" className={({ isActive }) =>
+                                isActive ? "font-bold " : ""
+                            } end>Business</NavLink>
+                            <NavLink to="/entertainment" className={({ isActive }) =>
+                                isActive ? "font-bold " : ""
+                            } end>Entertainment</NavLink>
+                            <NavLink to="/health" className={({ isActive }) =>
+                                isActive ? "font-bold " : ""
+                            } end>Health</NavLink>
+                            <NavLink to="/science" className={({ isActive }) =>
+                                isActive ? "font-bold " : ""
+                            } end>Science</NavLink>
+                            <NavLink to="/technology" className={({ isActive }) =>
+                                isActive ? "font-bold " : ""
+                            } end>Technology</NavLink>
+                            <NavLink to="/sports" className={({ isActive }) =>
+                                isActive ? "font-bold " : ""
+                            } end>Sports</NavLink>
+                        </div>
+                    </div>
+                    <div className="mr-6 flex gap-4">
+                        <NavLink to='/' end className={({ isActive }) =>
+                            isActive ? "font-bold " : ""
+                        }>Home</NavLink>
+                        <NavLink to='/about' className={({ isActive }) =>
+                            isActive ? "font-bold" : ""
+                        } end>About</NavLink>
+                        <img src={this.state.mode} onClick={() => {
                             this.modeHandler();
                             stylec();
-                        }
-                        }
-                             className="hover:cursor-pointer" alt="" />
+                        }} className="hover:cursor-pointer" alt="" />
                     </div>
                 </nav>
             </>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,6 +2,9 @@ import { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import LoadingBar from "react-top-loading-bar";
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 export default class News extends Component {
     api_key = import.meta.env.VITE_API_KEY
     constructor() {
@@ -10,6 +13,8 @@ export default class News extends Component {
             articles: [],
             loaded: false,
             page: 1,
+            tR: 0,
+            progress:0
         }
     }
     static defaultProps = {
@@ -17,58 +22,65 @@ export default class News extends Component {
         category: 'general'
     }
     static propTypes = {
-        title: PropTypes.string,
+        title: PropTypes.string
     }
-    capitalize = (text)=>{
-        return (text[0].toUpperCase()+text.slice(1))
+    capitalize = (text) => {
+        return (text[0].toUpperCase() + text.slice(1))
     }
-    update= async (pg=1)=>{
-        this.setState({ category: this.props.category })
-        const url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=${this.api_key}&page=${pg}&pageSize=6`
-        this.setState({ loaded: true })
+    fetchMore = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=${this.api_key}&page=${this.state.page+1}`
+        let res = await fetch(url);
+        let data = await res.json();
+        this.setState({
+            articles: this.state.articles.concat(data.articles),
+            page: this.state.page + 1,
+        })
+        console.log(data)
+        console.log("tr:", this.state.tR)
+        console.log("loaded:", this.state.articles.length + data.articles.length)
+    }
+    update = async () => {
+        this.setState({ category: this.props.category, loaded: true,progress:50 })
+        const url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=${this.api_key}&page=1`
         let res = await fetch(url);
         let data = await res.json();
         this.setState({
             articles: data.articles,
             loaded: false,
-            noOfPages: Math.ceil(data.totalResults / 6),
+            tR: data.totalResults,
+            progress:100
         })
         document.title = `${this.capitalize(this.props.category)} - NewsMonkey`
     }
-    prevClick = async () => {
-        this.setState({
-            page: this.state.page - 1,
-        })
-        this.update(this.state.page-1);
-    }
-    nextClick = async () => {
-        this.setState({
-            page: this.state.page + 1,
-        }),
-        this.update(this.state.page+1)
-    }
     componentDidMount() {
-        this.setState({
-            page: this.state.page
-       }),
         this.update();
+        this.setState({
+            progress:0
+        })
     }
     render() {
         let { styles, title } = this.props
         return (
             <>
+                <LoadingBar
+                color="#f11946"
+                progress={this.state.progress}
+                onLoaderFinished={() =>this.state.progress}
+                />
                 <div style={styles} className='w-[100vw] py-20'>
                     <div className='w-[80vw] mx-auto flex flex-col gap-7'>
-                        <div className='lg:text-3xl text-2xl mx-auto font-bold'>{title}</div>
-                        <div className='flex flex-wrap gap-7 justify-center items-center'>
-                            {this.state.loaded ? (<Spinner contSt='h-[70vh] grid place-content-center w-[100vw]' />) : (this.state.articles).map((ele) => (
-                                !this.state.loaded && <NewsItem key={ele.url} time={ele.publishedAt} auth={ele.author} url={ele.url} itemSt={styles.color === 'white' ? { backgroundColor: 'white', color: 'black' } : { backgroundColor: 'black', color: 'white' }} imgUrl={ele.urlToImage} title={ele.title} desc={ele.description} />))
-                            }
-                        </div>
-                        <div className='flex justify-between'>
-                            <button style={this.state.page > 1 ? { display: 'block' } : { visibility: 'hidden' }} onClick={this.prevClick} className='px-3 text-center rounded-xl py-1 text-white hover:cursor-pointer hover:bg-blue-500 bg-blue-600'>&larr; Previous</button>
-                            <button style={(this.state.page + 1 <= this.state.noOfPages) ? { visibility: "visible" } : { visibility: "hidden" }} onClick={this.nextClick} className='px-3 text-center rounded-xl py-1 text-white bg-blue-600 hover:bg-blue-500 hover:cursor-pointer'>Next &rarr;</button>
-                        </div>
+                        <div className='lg:text-3xl text-2xl mx-auto flex justify-center font-bold'>{title}</div>
+                        <InfiniteScroll
+                            dataLength={this.state.articles.length} //This is important field to render the next data
+                            next={this.fetchMore}
+                            hasMore={this.state.articles.length+this.state.page < this.state.tR}
+                            loader={<Spinner contSt="flex items-center justify-center mt-7" />}>
+                            <div className='flex flex-wrap gap-7 justify-center items-center'>
+                                {this.state.loaded ? (<Spinner contSt='h-[70vh] grid place-content-center w-[100vw]' />) : (this.state.articles).map((ele) => (
+                                    !this.state.loaded && <NewsItem key={ele.url} time={ele.publishedAt} auth={ele.author} url={ele.url} itemSt={styles.color === 'white' ? { backgroundColor: 'white', color: 'black' } : { backgroundColor: 'black', color: 'white' }} imgUrl={ele.urlToImage || ele.urlToImage !== "" ? ele.urlToImage : 'https://sportshub.cbsistatic.com/i/r/2025/02/11/b8507a2f-3f33-4c86-9dbe-51e8b4f00a43/thumbnail/1200x675/2c15065fcfee90a14f21dea07d0d7039/unctop25-1.jpg'} title={ele.title} desc={ele.description} />))
+                                }
+                            </div>
+                        </InfiniteScroll>
                     </div>
                 </div>
             </>
